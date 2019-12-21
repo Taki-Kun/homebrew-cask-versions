3eb35537f0af9b0d992d5e21b07ba97f07b96911
issenn <issennknight@gmail.com>
2019-12-22 06:05:13 +0800

    Initial commit

---
+++ Casks/mellow.rb
@@ -0,0 +1,19 @@
+cask 'mellow' do
+  version "0.1.11"
+  sha256 "be8e0f5ad3b80be2aa1bd3a2a98e34be6518f371c8ee2e476c149909d71f8e96"
+
+  url "https://github.com/mellow-io/mellow/releases/download/v#{version}/Mellow-#{version}.dmg"
+  appcast "https://github.com/mellow-io/mellow/releases.atom"
+  name "Mellow"
+  homepage "https://github.com/mellow-io/mellow"
+
+  depends_on macos: '>= :sierra'
+
+  app "Mellow.app"
+
+  zap trash: [
+    "~/Library/Application Support/Mellow",
+    "~/Library/Preferences/org.mellow.mellow.plist",
+    "~/Library/Logs/Mellow",
+  ]
+end
