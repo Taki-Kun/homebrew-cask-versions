16e5f15b47584190326ebf1b9888e9747d77b331
issenn <issennknight@gmail.com>
2020-01-31 21:50:06 +0800

    Initial commit

---
+++ Casks/keepvid-music-tag-editor.rb
@@ -0,0 +1,10 @@
+cask 'keepvid-music-tag-editor' do
+  version '3.0.0.2'
+  sha256 'a1927ccb86819cc2e651703b0657172183bd3a404fe8a28c4d1b5c1ce993b42a'
+
+  url "https://dl.bintray.com/issenn/homebrew-cask-versions/KeepVid_Music_Tag_Editor_#{version}.dmg"
+  name 'KeepVid Music Tag Editor'
+  homepage 'https://www.keepvid.info/'
+
+  app 'KeepVid Music Tag Editor.app'
+end
