796e8a90d11f9576319cdbc182616b2957be09c1
issenn <issennknight@gmail.com>
2020-01-31 20:52:52 +0800

    Initial commit

---
+++ Casks/squeed.rb
@@ -0,0 +1,10 @@
+cask 'squeed' do
+  version '1.13.2'
+  sha256 '8336e2aac2c9e7bc96f767dcda68686891e6c7209716faea0eed213b3f3b830d'
+
+  url "https://dl.bintray.com/issenn/homebrew-cask-versions/Squeed_#{version}.dmg"
+  name 'Squeed'
+  homepage 'https://beeb.li/squeed/'
+
+  app 'Squeed.app'
+end
