13b7d85ac569944256eab270b2d86684129d8e33
issenn <issennknight@gmail.com>
2020-01-31 21:32:42 +0800

    Initial commit

---
+++ Casks/music-tags.rb
@@ -0,0 +1,10 @@
+cask 'music-tags' do
+  version '1.0.0'
+  sha256 '44f6556bfdac9be7353302b556f320285229aaaca8df5e1d562b641af65fd865'
+
+  url "https://dl.bintray.com/issenn/homebrew-cask-versions/Music_Tags_#{version}.dmg"
+  name 'Music Tags'
+  homepage 'https://apps.apple.com/app/id1395905089'
+
+  app 'Music Tags.app'
+end
