68e8c56ac6db6768dee13c1da97df35a9686be30
issenn <issennknight@gmail.com>
2020-01-31 20:05:45 +0800

    Initial commit

---
+++ Casks/tagr.rb
@@ -0,0 +1,10 @@
+cask 'tagr' do
+  version '5.1.0.1'
+  sha256 'b0e724bae157b6274cd6a00a8c33caa68d7d9ed38e3f9c8e9f5fd015a8864d3f'
+
+  url "https://dl.bintray.com/issenn/homebrew-cask-versions/Tagr_#{version}.dmg"
+  name 'Tagr'
+  homepage 'http://entwicklungsfreu.de/tagr.html'
+
+  app 'Tagr.app'
+end
