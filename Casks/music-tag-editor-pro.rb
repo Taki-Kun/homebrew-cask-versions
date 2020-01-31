b4f26d1d923dbcd7593e53309e86edfe4764371e
issenn <issennknight@gmail.com>
2020-01-31 18:15:45 +0800

    Initial commit

---
+++ Casks/music-tag-editor-pro.rb
@@ -0,0 +1,10 @@
+cask 'music-tag-editor-pro' do
+  version '4.0.1'
+  sha256 'c0431ed46aa75463c93d0f68eff5f285fda37eb71cfcca2183a9bec4049e3670'
+
+  url "https://dl.bintray.com/issenn/homebrew-cask-versions/Music_Tag_Editor_Pro_#{version}.dmg"
+  name 'Music Tag Editor Pro'
+  homepage 'http://www.yumysoft.com/'
+
+  app 'Music Tag Editor Pro.app'
+end
