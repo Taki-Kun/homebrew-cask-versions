d2ae84af4b2934c6b1a34ace3db33e26b57ad52b
issenn <issennknight@gmail.com>
2020-01-31 16:16:44 +0800

    Initial commit

---
+++ Casks/metadatics.rb
@@ -0,0 +1,10 @@
+cask 'metadatics' do
+  version '1.6'
+  sha256 '5dbad8cab1af73a33983565cab6b8975327c76e4b7b8ca90afb7a8167ac6a72e'
+
+  url "https://dl.bintray.com/issenn/homebrew-cask-versions/metadatics_#{version}.dmg"
+  name 'Metadatics'
+  homepage 'https://www.markvapps.com/metadatics'
+
+  app 'Metadatics.app'
+end
