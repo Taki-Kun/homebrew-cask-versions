14f98b0441c13d1483c5b6808fcad2944a16af72
issenn <issennknight@gmail.com>
2020-03-22 04:26:32 +0800

    Add squirrel

---
+++ Casks/squirrel.rb
@@ -0,0 +1,28 @@
+cask 'squirrel' do
+  version '0.14.0,a2c0348'
+  sha256 '3381f5678c1ea8d5b01eeb1a8767b027b83e40f70896833028638e61f2f385da'
+
+  # dl.bintray.com/rime/squirrel was verified as official when first introduced to the cask
+  url "https://dl.bintray.com/rime/squirrel/Squirrel-#{version.before_comma}+git#{version.after_comma}.zip"
+  appcast 'https://rime.im/release/squirrel/appcast.xml'
+  name 'Squirrel'
+  homepage 'https://rime.im/'
+
+  auto_updates false
+
+  pkg 'Squirrel.pkg'
+
+  uninstall pkgutil: [
+                       'im.rime.inputmethod.Squirrel',
+                       'com.googlecode.rimeime.Squirrel.pkg', # Package name of older versions (< 0.10.0)
+                     ],
+            delete:  '/Library/Input Methods/Squirrel.app'
+
+  zap trash: [
+               '~/Library/Caches/im.rime.inputmethod.Squirrel',
+               '~/Library/Preferences/im.rime.inputmethod.Squirrel.plist',
+               # Data for older versions (< 0.10.0)
+               '~/Library/Caches/com.googlecode.rimeime.inputmethod.Squirrel',
+               '~/Library/Preferences/com.googlecode.rimeime.inputmethod.Squirrel.plist',
+             ]
+end
