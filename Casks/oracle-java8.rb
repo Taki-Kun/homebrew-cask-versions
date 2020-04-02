5985f1c0dc43a28f1c1311b09be4b0543952a873
issenn <issennknight@gmail.com>
2020-04-02 15:52:40 +0800

    Initial commit

---
+++ Casks/oracle-java8.rb
@@ -0,0 +1,28 @@
+cask 'oracle-java8' do
+  version '8u241'
+  sha256 'b41367948cf99ca0b8d1571f116b7e3e322dd1ebdfd4d390e959164d75b97c20'
+
+  url "http://localhost/jdk-#{version}-macosx-x64.dmg"
+  name 'Oracle Java 8 Standard Edition Development Kit'
+  homepage 'https://www.oracle.com/technetwork/java/javase/overview/index.html'
+
+  depends_on macos: '>= :yosemite'
+
+  pkg 'JDK 8 Update 241.pkg'
+
+  uninstall pkgutil: "com.oracle.jdk#{version.before_comma}",
+            delete:  [
+                       '/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin',
+                       '/Library/PreferencePanes/JavaControlPanel.prefPane',
+                     ]
+
+  zap trash: [
+               '~/Library/Application Support/Oracle/Java',
+               '~/Library/Application Support/com.oracle.java.JavaAppletPlugin.plist',
+               '~/Library/Application Support/com.oracle.javadeployment.plist',
+             ]
+
+  caveats do
+    license 'https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html'
+  end
+end
