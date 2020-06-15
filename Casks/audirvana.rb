f21ef9785ec432adc906625285cf49888c8db757
issenn <issennknight@gmail.com>
2020-06-15 10:48:17 +0800

    Update: Audirvana

--- Casks/audirvana.rb
+++ Casks/audirvana.rb
@@ -1,6 +1,6 @@
 cask 'audirvana' do
-  version '3.5.29.3559'
-  sha256 'f9958f48495c80e669a30f2783f5cb1880c7ef0e6b2efdb312e16f4d229a35e7'
+  version '3.5.37.3567'
+  sha256 'aa02f607f2790c26cde816186d704e8b9f85d404d8cf8719ff2d02247d61cc46'
 
   url "https://dl.bintray.com/issenn/homebrew-cask-versions/Audirvana_#{version}.dmg"
   appcast "https://audirvana.com/delivery/audirvana#{version.major}_#{version.minor}_appcast.xml"
